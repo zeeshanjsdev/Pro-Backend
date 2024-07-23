6c8523b0afe6c513520160726c4b12929c595474
ZZ code <iamzeeshan00@gmail.com>
2024-07-23 22:33:28 +0500

    utilscode

--- src/index.js
+++ src/index.js
@@ -10,7 +10,19 @@ dotenv.config({
 })
 
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server is running at pot : ${process.env.PORT}`);
+    app.on('error', (error)=>{
+        console.log('Error :', error);
+        throw error
+    })
+    })
+})
+.catch((error)=>{
+    console.log("Mongo db connection failed")
+})
 
 
 
