070ed2be46d93ccba62d6639f584949c47d68a5b
Zeeshan <143320208+zeeshanjsdev@users.noreply.github.com>
2024-07-24 09:17:59 +0500

    Update apiResponse.js

--- utils/apiResponse.js
+++ utils/apiResponse.js
@@ -1,8 +1,10 @@
-class ApiResponse{
-    constructor(statusCode, data, message = "Success"){
-            this.statusCode = statusCode
-            this.data = data
-            this.message = message
-            this.statusCode = statusCode < 400
-    }
+class ApiResponse {
+  constructor(statusCode, data, message = "Success") {
+    this.statusCode = statusCode;  // HTTP status code
+    this.data = data;  // The actual data being returned
+    this.message = message;  // A message describing the status or outcome
+    this.success = statusCode < 400;  // Indicates if the status code represents a successful response
+  }
 }
+
+export { ApiResponse };
