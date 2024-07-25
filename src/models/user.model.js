39c56edc74e40ffc341bf538e0740c33ab2dbcd9
Zeeshan Amjad <143320208+zeeshanjsdev@users.noreply.github.com>
2024-07-25 07:16:35 +0500

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -52,7 +52,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
   if (!this.isModified("passWord")) next();
 
-  this.password = bcrypt.hash(this.passWord, 10);
+  this.password = await bcrypt.hash(this.passWord, 10);
   next();
 });
 
