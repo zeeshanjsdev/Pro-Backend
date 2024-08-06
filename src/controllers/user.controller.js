6d191736d834fb56efba428014c56131e2191b8c
ZZ code <iamzeeshan00@gmail.com>
2024-08-06 23:42:24 +0500

    fixed bugs and added assigment

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,9 @@
 import { asyncHandler } from "../../utils/asyncHandler.js";
-import { ApiError } from "../../utils/apiError.js";
+import { ApiError } from "../../utils/ApiError.js";
 import { User } from "../models/user.model.js";
 import { uploadOnCloudinary } from "../../utils/cloudinary.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { ApiResponse } from "../../utils/apiResponse.js";
+import { ApiResponse } from "../../utils/ApiResponse.js";
 import { response } from "express";
 import { SchemaTypeOptions } from "mongoose";
 import jwt from "jsonwebtoken";
